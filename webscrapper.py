f65c281d53b6a41f8f6f21ee3822dcb56d763d06
Adrianna Kaminska <adrianna.kaminska@bbc.co.uk>
2021-01-08 18:03:06 +0000

    loop works, yay

--- webscrapper.py
+++ webscrapper.py
@@ -10,18 +10,16 @@ finalWriter = csv.writer(open("dataFromKrolowiePolscy.csv", "w")) #the output fi
 finalWriter.writerow(["Name", "The beginning of the reign", "The end of the reign", "Parents"]) #first row - column names
 
 filtered_table = soup.find_all('table', class_="wikitable")
-
 for tr in filtered_table:
-  tds = tr.find_all("td")
-
-  try:
-    names = str(tds[2].get_text())
-    theBeginning = str(tds[6].get_text())
-    theEnd = str(tds[7].get_text())
-    parents = str(tds[8].get_text())
-  except:
-    print("bad tr string: {}".format(tds))
-    continue
-  finalWriter.writerow([names, theBeginning, theEnd, parents]) #oh boy, it works
-
-
+  trs = tr.find_all('tr')
+  for tr2 in trs:
+    tds = tr2.find_all('td')
+    try:
+      names = str(tds[1].get_text())
+      theBeginning = str(tds[5].get_text())
+      theEnd = str(tds[6].get_text())
+      parents = str(tds[7].get_text())
+    except:
+      print("bad tr string: {}".format(tds))
+      continue
+    finalWriter.writerow([names, theBeginning, theEnd, parents]) #oh boy, it works
